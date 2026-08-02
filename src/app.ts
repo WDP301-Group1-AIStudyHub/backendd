264d761c3fa0d5aa3960fd77b41c6ef171ba545c
Khang Duy <dokhangkhang0410@gmail.com>
2026-08-02 16:15:18 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,8 @@ const allowedOrigins = new Set(
   [
     process.env.CLIENT_URL,
     process.env.FRONTEND_URL,
+    "http://localhost:4000",
+    "http://127.0.0.1:4000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8081",
